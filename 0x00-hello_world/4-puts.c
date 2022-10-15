e14172ac9cf661c2254d3a076f37ab8160ccbb46
Chebem Eze <chebemeze@gmail.com>
2022-10-15 06:49:39 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ *
+ * main - It prints Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(0)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
