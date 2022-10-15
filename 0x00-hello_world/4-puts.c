3bb06e54fe73032a602696c8f72eabb38fc3140d
Chebem Eze <chebemeze@gmail.com>
2022-10-15 07:55:42 -0700

    modied 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
+/**
  *
- * main - It prints Programming is like building a multilingual puzzle
- *
+ * main - It prints programming is like building a 
+ *		multilingual puzzle
  * Return: Always 0 (Success)
  */
-
-int main(0)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
