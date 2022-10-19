c4c1c9a9875f8f633209a4353ad7860a57de70e9
Chebem Eze <chebemeze@gmail.com>
2022-10-18 22:33:07 -0700

    ammended files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- *_islower - Returns "1" if letter entered is lowercase
+ * _islower - Returns "1" if letter entered is lowercase
  * and returns "0" when an uppercase isentered.
+ * @c: single letter input.
+ * Return: 1 if int c is lowercase.
  */
-int _islower( int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else if(c >= 'A' && c <= 'Z')
+	else
 	{
-		return (0);
-	}
-	else {
 		return (0);
 	}
 }
