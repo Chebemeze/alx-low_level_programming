881ec12444d6cf0da1335cee6a56673f6ce6c42d
Chebem Eze <chebemeze@gmail.com>
2022-10-20 16:21:46 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 omitting 2 and 4.
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar((i % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
