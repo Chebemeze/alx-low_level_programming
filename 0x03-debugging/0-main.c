843d1926f944166f46fd41e86ccc453230c0bfc0
Chebem Eze <chebemeze@gmail.com>
2022-10-20 03:05:52 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks fucntion to se if it produces the right result
+ * when given a value of 0.
+ * Return: returns 0 if successful.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
