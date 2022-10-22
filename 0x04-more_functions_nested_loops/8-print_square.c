299907d4e908bd624049481640e7eae908a2f623
Chebem Eze <chebemeze@gmail.com>
2022-10-21 21:40:58 -0700

    prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square when given a single number
+ * @size: the size of the square
+ */
+
+void print_square(int size);
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+
+				if (x == size - 1)
+				{
+					continue;
+				}
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
