bf98185668ed6d7efe963a46e425bff7a03c8dde
Chebem Eze <chebemeze@gmail.com>
2022-10-25 20:48:56 -0700

    multiple of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints members of an array who are
+ * muktiple of 2
+ * @str: Array given.
+ *
+ */
+
+void puts2(char *str)
+{
+	j = 0;
+
+	while (str[j])
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
