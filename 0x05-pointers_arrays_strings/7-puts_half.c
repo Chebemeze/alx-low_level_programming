5d7ff093461108c4c901786819c805a60420f4ff
Chebem Eze <chebemeze@gmail.com>
2022-10-25 21:44:12 -0700

    primts half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * puts_half - prints the other half of string given
+ * @str: the string givien
+ *
+ */
+
+void puts_half(char *str)
+{
+	int k, l;
+
+	while (str[k])
+	{
+		k++;
+	}
+	k--;
+	l = k / 2;
+	k = 0;
+	while (str[k])
+	{
+		if (k > l)
+		{
+			_putchar(str[k]);
+		}
+	}
+	-putchar('\n');
+}
