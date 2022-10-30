ac06c1d0073952c40ec3a037d1da658fd0f011cc
Chebem Eze <chebemeze@gmail.com>
2022-10-30 14:55:13 -0700

    capitalizes words except for word seperators

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes words found after seprator of words(e.g "," "\n"
+ * "!!" etc)
+ * @e: point variable
+ * Return: returns the new capitalized string.
+ */
+
+char *cap_string(char *e)
+{
+	int j;
+	char tab;
+
+	tab = '\0';
+	j = 0;
+	while (e[j])
+	{
+		tab = e[j - 1];
+		if (e[j] >= 'a' && e[j] <= 'z')
+		{
+			if (tab == '\n' || tab == ' ' || tab == ',' || tab == '!')
+			{
+				e[j] = e[j] - 32;
+			}
+			else if (tab == ';' || tab == '\t' || tab == '{' || tab == '}')
+			{
+				e[j] = e[j] - 32;
+			}
+			else if (tab == ')' || tab == '(' || tab == '.' || tab == 0)
+			{
+				e[j] = e[j] - 32;
+			}
+		}
+		j++
+	}
+
+	return (p);
+}
