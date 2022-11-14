83815462229970e4f727afd341ffe504fa596c4b
Chebem Eze <chebemeze@gmail.com>
2022-11-14 04:57:05 -0800

    A function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code.
+ *
+ * Return: Always 0(if successful).
+ */
+
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+
+	return (0);
+}
