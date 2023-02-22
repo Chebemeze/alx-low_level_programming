5debf0a973990af6fb644e2e0f13b8804680e9c6
Chebem Eze <chebemeze@gmail.com>
2023-02-22 15:36:51 -0800

    irst 50 fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbrs
+ * Return: Return 0 if successsful
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long j = 1, increase_num  = 2, k;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i < 48)
+		{
+			printf("%lu, ", j);
+		}
+		else
+		{
+			printf("%lu", j);
+		}
+		k = j;
+		j = increase_num;
+		increase_num = j + k;
+	}
+	printf("\n");
+
+	return (0);
+}
