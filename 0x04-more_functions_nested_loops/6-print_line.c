2794af7546594ba98b1c6b2e793644f72539005e
Chebem Eze <chebemeze@gmail.com>
2023-02-23 14:38:39 -0800

    Draws a straight line on the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n < 0 || n == 0)
+	{
+		_putchar ('\n');
+	}
+	else if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
-	}
-	else if (n < 0 || n == 0)
-	{
-		_putchar ('\n');
 	}
 }
