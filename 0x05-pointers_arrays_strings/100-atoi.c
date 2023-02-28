a39b5cf8b187d2050e786351f57694d3b5b34752
Chebem Eze <chebemeze@gmail.com>
2023-02-28 10:14:00 -0800

    A function that converts string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _atoi - converts string to integer
+ * @s: string to be converted
+ * Return: returns the complete integer(taking into conside
+ * ration sign convention before the string)
+ */
+
+int _atoi(char *s)
+{
+	int i, b, precedent, total;
+
+	precedent = 0;
+	total = 0;
+	b = strlen(s);
+
+	for (i = 0; i < b; i++)
+	{
+		if (s[i] == ';')
+		{
+			break;
+		}
+		if (isdigit(s[i]))
+		{
+      			total = (total * 10) + s[i] - '0';
+ 		}
+		if (s[i] == '+')
+		{
+			precedent++;
+		}
+		else if (s[i] == '-')
+		{
+			precedent--;
+		}
+	}
+	if (precedent < 0)
+	{
+		total = -total;
+	}
+	else if (precedent > 0)
+	{
+		total = total;
+	}
+
+	return (total);
+}
