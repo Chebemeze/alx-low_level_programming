f1fa8437e32aa41c59944800ea9a87bb9f2ed6d2
Chebem Eze <chebemeze@gmail.com>
2023-03-01 14:59:10 -0800

    encodes a string into rot13 format

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - A function that encodes a given string
+ * to the "ROT-13" format
+ * @s: the string given
+ * Return: returns the encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char e[] = "AbcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char z[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (e[j] == *(s + i))
+			{
+				*(s + i) = z[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
