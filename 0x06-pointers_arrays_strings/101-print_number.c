7acf7668eadfea00ac65aff2111ae405767c997a
Chebem Eze <chebemeze@gmail.com>
2023-03-03 06:20:42 -0800

    A function that prints numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number withouth the use of array or pointers
+ * @n: the number to be printed
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int division_num, j;
+
+	division_num = 1;
+	if (n < 0)
+	{
+	j = -n;
+	_putchar('-');
+	}
+	else
+	{
+		j = n;
+	}
+
+	while (j >= 10)
+	{
+		division_num = division_num * 10;
+		j = j / 10;
+	}
+	while (division_num > 0)
+	{
+		_putchar(((n / division_num) % 10) + '0');
+		division_num = division_num / 10;
+	}
+}
