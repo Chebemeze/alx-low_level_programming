7a2d0f9aff47c7e347a3e0f2fa605000f9598e8f
Chebem Eze <chebemeze@gmail.com>
2023-03-10 14:40:50 -0800

    fixed eror

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,9 @@
 /**
  * main - checks the code. prints the name of executable
  * fie.
+ * @argc: This the total count of arguments.
+ * @argv: This is a pointer pointing to the array of
+ * arguments when the code is executed.
  * Return: 0 if successful.
  */
 
