9725a2fa10f21294f34861649da37ff7f90b7f39
Chebem Eze <chebemeze@gmail.com>
2023-03-19 10:34:20 -0700

    Allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
 	p = calloc(nmemb, size);
-
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	return (p);
 }
