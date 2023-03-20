1ea164cbe3bad1ed448959122095f41d06c86673
Chebem Eze <chebemeze@gmail.com>
2023-03-20 07:35:31 -0700

    fixed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		newb = malloc(new_size);
 		if (newb == NULL)
 			return (NULL);
-		free(ptr);
+
+		return (newb);
 	}
 	if (ptr != NULL && new_size == 0)
 	{
