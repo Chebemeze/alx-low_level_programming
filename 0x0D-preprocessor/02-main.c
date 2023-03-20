26d19a586468faaaa56aad4882480cc8524eae30
Chebem Eze <chebemeze@gmail.com>
2023-03-20 14:48:35 -0700

    prints a files name using predefined macro

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of this file
+ *
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	printf("\n");
+
+	return (0);
+}
