bdd287e41fefee2f38eeb278b90e221060e3d4e0
Chebem Eze <chebemeze@gmail.com>
2023-03-21 15:20:43 -0700

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,10 +11,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	int i, j;
+
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
+
+	/**
+	 * gets the length of string name
+	 */
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	ptr->name = malloc(sizeof(char) * (i));
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	/**
+	 * gets the length of string owner
+	 */
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	ptr->owner = malloc(sizeof(char) * (j));
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+
 	(*ptr).name = name;
 	(*ptr).age = age;
 	(*ptr).owner = owner;
