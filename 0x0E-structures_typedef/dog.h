ed239a70d1ef9cae82c094a7d9e3f711af64cf00
Chebem Eze <chebemeze@gmail.com>
2023-03-20 22:37:33 -0700

    initializes struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
 #ifndef DOG_H_
 #define DOG_H_
 
-struct dogg
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
