b98fbadfbb844f032ff6d85af9c6159986c82cdd
Chebem Eze <chebemeze@gmail.com>
2023-03-26 19:23:50 -0700

    ammended code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,8 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(num, int));
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
-		if (i == n - 1)
-			printf("\n");
 	}
+	printf("\n");
+
 	va_end(num);
 }
