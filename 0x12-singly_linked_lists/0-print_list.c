0d9ff3e5ccfbd81c6a9210cc8ee2dbf73f96a4a0
Chebem Eze <chebemeze@gmail.com>
2023-03-31 07:39:51 -0700

    modified

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] [%s]\n", h->len, h->str);
 
-		h = h->next;
 		num_node++;
+		h = h->next;
 	}
 
 	return (num_node);
