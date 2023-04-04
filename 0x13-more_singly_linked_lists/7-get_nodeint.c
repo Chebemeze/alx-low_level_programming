c4c18ba399f5d554d6119ebf3235365aab0bf35e
Chebem Eze <chebemeze@gmail.com>
2023-04-04 16:40:45 -0700

    ammended code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (numb < index)
 	{
 		if (head->next == NULL)
-			return (0);
+			return (NULL);
 		p = head->next;
 		head = head->next;
 		numb++;
