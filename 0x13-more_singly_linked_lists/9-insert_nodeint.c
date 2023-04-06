4b1bbad15986f57ea4b8f07261890ca2f1fa4385
Chebem Eze <chebemeze@gmail.com>
2023-04-05 19:45:47 -0700

    fix invalid index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		secondptr = secondptr->next;
 		numb++;
 	}
-	if (secondptr == NULL)
+	if (secondptr->next == NULL)
 		return (NULL);
 
 	p = malloc(sizeof(listint_t));
