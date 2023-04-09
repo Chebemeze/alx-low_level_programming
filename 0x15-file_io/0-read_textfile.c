5c3b3654788feb4ff9bbb7b3425a96c273ac81a5
Chebem Eze <chebemeze@gmail.com>
2023-04-09 09:58:47 -0700

    reads and print the content of a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output.
+ *
+ * @filename: pointer to the file name.
+ *
+ * @letters: the number of letters it should read and print.
+ *
+ * Return: The number of letters printed if successful.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ptr;
+	ssize_t i, j, l;
+
+	if (filename == NULL)
+		return (0);
+
+	ptr = malloc(sizeof(char) * letters);
+	if (ptr == NULL)
+		return (0);
+
+	j = open(filename, O_RDWR);
+	if (j == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	i = read(j, ptr, letters);
+	if (i == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	l = write(STDOUT_FILENO, ptr, i);
+	if (l == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	free(ptr);
+	close(j);
+	return (l);
+}
